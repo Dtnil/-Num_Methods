e1ccb46237c6f759d0a5035cb83a2fd9ace07c9e
OLD_terent <106813492+Dtnil@users.noreply.github.com>
2026-03-15 19:50:52 +0200

    Add files via upload

---
+++ Equations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public static class Equations
+{
+    public static double F(int id, double x)
+    {
+        switch (id)
+        {
+            case 1: return Math.Cos(Math.Sin(Math.Pow(x, 3))) - 0.7;
+            case 2: return x * x - 2 * x - 4;
+            case 3: return Math.Sin(Math.Pow(x, 3)) / 5.0;
+            case 4: return Math.Pow(x, 3) + x * x - 4 * x - 2;
+            case 5: return x * x * Math.Cos(x) - 0.2;
+            case 6: return 1.5 - Math.Pow(x, 1 - Math.Cos(x));
+            case 7: return Math.Pow(x, 3) - 3 * x + 1.5;
+            case 8: return Math.Cos(x * x - x + 1);
+            case 9: return Math.Exp(x) - Math.Sin(2 * x) - 1;
+            case 10: return (Math.Pow(x, 3) - 7 * x + 1) / 3.0;
+            case 11: return Math.Cos(x * x) - 0.5;
+            case 12: return Math.Sin(x) - 0.5 * Math.Cos(x * x);
+            case 13: return Math.Log(Math.Sin(2 * x) + 1);
+            case 14: return Math.Sin(x) - Math.Cos(Math.Pow(x, 3));
+            default: return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Виберіть рівняння:");
+
+        Console.WriteLine("1  cos(sin(x^3)) - 0.7");
+        Console.WriteLine("2  x^2 - 2x - 4");
+        Console.WriteLine("3  sin(x^3) / 5");
+        Console.WriteLine("4  x^3 + x^2 - 4x - 2");
+        Console.WriteLine("5  x^2 cos(x) - 0.2");
+        Console.WriteLine("6  1.5 - x^(1-cos(x))");
+        Console.WriteLine("7  x^3 - 3x + 1.5");
+        Console.WriteLine("8  cos(x^2 - x + 1)");
+        Console.WriteLine("9  e^x - sin(2x) - 1");
+        Console.WriteLine("10 (x^3 - 7x + 1) / 3");
+        Console.WriteLine("11 cos(x^2) - 0.5");
+        Console.WriteLine("12 sin(x) - 0.5 cos(x^2)");
+        Console.WriteLine("13 ln(sin(2x)+1)");
+        Console.WriteLine("14 sin(x) - cos(x^3)");
+
+        int eq = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введіть початок інтервалу:");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введіть кінець інтервалу:");
+        double b = double.Parse(Console.ReadLine());
+
+        RootFinder solver = new RootFinder();
+
+        List<double> roots = solver.FindRoots(eq, a, b);
+
+        Console.WriteLine("Знайдені корені:");
+
+        foreach (double r in roots)
+        {
+            Console.WriteLine(Math.Round(r, 3));
+        }
+
+        Console.ReadLine();
+    }
+}
