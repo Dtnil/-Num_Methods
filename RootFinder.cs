9ca9d159163b120833fed7044b37c11372d64452
OLD_terent <106813492+Dtnil@users.noreply.github.com>
2026-03-17 10:12:20 +0200

    Refactor RootFinder class and clean up code

--- RootFinder.cs
+++ RootFinder.cs
@@ -48,43 +48,7 @@ public class RootFinder
         }
             return roots;
     }
-    //public List<double> FindRoots(int eq, double a, double b)
-    //{
-    //    List<double> roots = new List<double>();
-
-    //    double step = (b - a) / 10000.0;
-
-    //    for (double x = a; x < b; x += step)
-    //    {
-    //        double x1 = x;
-    //        double x2 = x + step;
-
-    //        double f1 = Equations.F(eq, x1);
-    //        double f2 = Equations.F(eq, x2);
-
-    //        if (f1 * f2 <= 0)
-    //        {
-    //            double root = Chord(eq, x1, x2);
-
-    //            bool exists = false;
-
-    //            foreach (double r in roots)
-    //            {
-    //                if (Math.Abs(r - root) < eps)
-    //                {
-    //                    exists = true;
-    //                    break;
-    //                }
-    //            }
-
-    //            if (!exists)
-    //                roots.Add(root);
-    //        }
-    //    }
-
-    //    return roots;
-    //}
-
+    
     double Chord(int eq, double a, double b)
     {
         double x;
