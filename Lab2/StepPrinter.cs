5eca2c47359fd2f3abceb7dcb67f8fa89e8c9227
OLD_terent <106813492+Dtnil@users.noreply.github.com>
2026-03-22 16:28:58 +0200

    Add files via upload

---
+++ Lab2/StepPrinter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class StepPrinter
+{
+    public static void PrintMatrix(string name, double[,] matrix, int n)
+    {
+        Console.WriteLine($"\n{name}:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+                Console.Write($"{matrix[i, j],8:F2}");
+            Console.WriteLine();
+        }
+    }
+
+    public static void PrintVector(string name, double[] vector)
+    {
+        Console.WriteLine($"\n{name}:");
+        for (int i = 0; i < vector.Length; i++)
+            Console.WriteLine($"{name}[{i}] = {vector[i]:F2}");
+    }
+
+    public static void PrintStep(string text)
+    {
+        Console.WriteLine("\n--- " + text + " ---");
+    }
+}
